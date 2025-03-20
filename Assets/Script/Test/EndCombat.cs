7fc523deb7fefc387ef838311021459d548fe954
ElChagnon <92257859+ElChagnon@users.noreply.github.com>
2025-03-20 21:34:43 +0100

    Ajout Support Clavier dans Overworld + IA Ennemie
    -Support Clavier dans Overworld
    -Lancement de Combat en appuyant sur C
    -Ajout d'une limite de soin
    -Amélioration de IA Ennemi

---
+++ Assets/Script/Test/EndCombat.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndCombat : MonoBehaviour
+{
+    public void ReturnToWorld()
+    {
+        SceneManager.LoadScene("World");
+    }
+}
---
+++ Assets/Script/Test/RandomEncounter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class RandomEncounter : MonoBehaviour
+{
+    public float encounterRate = 0.5f; // 50% de chance de déclencher un combat
+
+    void Update()
+    {
+        // Vérifier si la touche "C" est pressée
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            CheckForEncounter();
+        }
+    }
+
+    void CheckForEncounter()
+    {
+        // Générer un nombre aléatoire pour déterminer si un combat est déclenché
+        if (Random.value < encounterRate)
+        {
+            TriggerCombat();
+        }
+        else
+        {
+            Debug.Log("Pas de combat cette fois-ci !");
+        }
+    }
+
+    void TriggerCombat()
+    {
+        // Charger la scène de combat
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Battle");
+    }
+}
